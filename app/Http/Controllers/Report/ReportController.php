5f34c47c4ff8df93c6ea174e9ede364653609c50
Rafael Gamez Diaz <rafaelgamezdiaz@gmail.com>
2020-03-16 15:21:36 -0400

    Change report funtionality due tu changes in pagination

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -9,7 +9,10 @@
 namespace App\Http\Controllers\Report;
 
 use App\Http\Controllers\Controller;
+use App\Services\ClientService;
+use App\Services\PaymentService;
 use App\Services\ReportService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -28,24 +31,61 @@ class ReportController extends Controller
     /**
      *  Returns the Payments Report
      */
-    public function automatic(Request $request)
+    public function automatic(Request $request,
+                              PaymentService $paymentService,
+                              ClientService $clientService,
+                              UserService $userService)
     {
-        $index= [$request->index];
-       // $info = $this->sortByDate([$request->data]);
-        $info = [$request->data];
+        $index = [
+            'ITEM'                  =>'id',
+            'Tipo de pago'          =>'type',
+            'Cliente'               =>'client',
+            'Documento'             =>'reference',
+            'Usuario'               =>'username',
+            'Monto del Pago'        =>'amount',
+            'Forma de pago'         =>'method',
+            'Monto Disponible'      =>'amount_pending',
+            'Fecha/Hora de pago'    =>'created_at',
+            'Estado'                =>'status'
+        ];
+        $payments = $paymentService->index($request, $clientService, $userService, true);
+        $info = $this->buildReportTable($payments);
         $report = (new ReportService());
         $report->indexPerSheet($index);
         $report->dataPerSheet($info);
-        $report->data($request->data);
-        $report->index($request->index);
+        $report->data($info);
+        $report->index($index);
         $report->external();
         $report->transmissionRaw();
 
         // Load Logo
         $user = $request->get('user')->user;
         $report->getAccountInfo($user->current_account);
+        $report->username($user->username);
 
         return $report->report("automatic","Pagos","",null,false,2);
+    }
+
+    private function buildReportTable($payments){
+        $table = array();
+        $payments = collect($payments)->recursive();
+        $item = 1;
+        foreach ($payments as $payment){
+                array_push($table, [
+                    'id'            => $item,
+                    'type'          => $payment['type']['type'],
+                    'client'        => $payment['client']['commerce_name'],
+                    'reference'     => $payment['reference'],
+                    'username'      => $payment['user']['name'],
+                    'amount'        => $payment['amount'],
+                    'method'        => $payment['method']['method'],
+                    'amount_pending' => $payment['amount_pending'],
+                    'created_at'    => $payment['created_at'],
+                    'status'      => $payment['status']
+                ]);
+                $item++;
+        }
+        return $table;
     }
 
 }
